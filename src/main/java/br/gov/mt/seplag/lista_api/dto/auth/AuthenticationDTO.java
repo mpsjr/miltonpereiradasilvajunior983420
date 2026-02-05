9dfd5aef4b063d2bede6ba4046c5ea7d30e2ec2b
Milton Pereira da Silva Júnior <mpsjunior@gmail.coma>
2026-02-05 19:27:15 -0400

    commit atualizado AuthenticationDTO para facilitar login
    - adiciona pré-preenchimento de email e senha no endpoint de login

--- src/main/java/br/gov/mt/seplag/lista_api/dto/auth/AuthenticationDTO.java
+++ src/main/java/br/gov/mt/seplag/lista_api/dto/auth/AuthenticationDTO.java
@@ -1,8 +1,14 @@
 package br.gov.mt.seplag.lista_api.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record AuthenticationDTO(
-        @NotBlank String email, 
-        @NotBlank String senha
+        @Schema(description = "E-mail do usuário cadastrado", example = "admin@seplag.mt.gov.br")
+        @NotBlank 
+        String email,
+
+        @Schema(description = "Senha de acesso", example = "123")
+        @NotBlank 
+        String senha
 ) {}
